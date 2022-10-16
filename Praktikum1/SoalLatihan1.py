ce70144f188914251ca69c0c013cb6602952797b
Su0h <55811146+AnkaFayiz@users.noreply.github.com>
2022-10-16 19:32:54 +0700

    Updated

---
+++ Praktikum1/SoalLatihan1.py
@@ -0,0 +1,7 @@
+nama = input("Nama kamu siapa? ")
+asal = input(f'Darimana asal kamu {nama}? ')
+asal_sekolah = input(f'Sebelumnya bersekolah dimana? ')
+
+print(f'\n\nHello selamat siang {nama}')
+print(f'Wah dari {asal}')
+print(f'Pasti menyenangkan bersekolah di {asal_sekolah}\n')
